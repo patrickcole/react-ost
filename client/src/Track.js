dcff41edff484b2fb7ff3665ffaae9d5881aad11
Patrick Cole <hpcmobiledev@gmail.com>
2019-07-02 14:29:07 -0400

    Tracks display duration lengths and when selected, scroll to top of page

--- client/src/Track.js
+++ client/src/Track.js
@@ -5,19 +5,18 @@ function Track( { data } ) {
 
   const { player } = useContext(PlayerContext);
 
-  let seekPlayerTo = (e) => player.seekTo(e.target.dataset.seconds);
+  let onTrackSelected = (e) => {
+    seekPlayerTo(e.target.dataset.seconds);
+    window.scrollTo(0,0);
+  }
+  let seekPlayerTo = (seconds) => player.seekTo(seconds);
   let formatDuration = (duration) => new Date(1000 * duration).toISOString().substr(11,8);
 
   return (
     <li className="list-item__track">
-      <button className="track" onClick={seekPlayerTo} data-seconds={data.playAt}>
-        {data.title}
-        {
-        /* 
+      <button className="track" onClick={onTrackSelected} data-seconds={data.playAt}>
         <span className="track__title">{data.title}</span>
         <span className="track__duration">{formatDuration(data.duration)}</span>
-        */
-        }
       </button>
     </li>
   )
